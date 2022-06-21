6ee40b90a271362e8fd683d76e386154a653ec6c
Herve2iko <ihervedelice@gmail.com>
2022-06-21 17:40:14 +0200

    clientControl

---
+++ src/main/java/com/herve2iko/relation/Controller/ClientController.java
@@ -0,0 +1,33 @@
+package com.herve2iko.relation.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.herve2iko.relation.Entity.Client;
+import com.herve2iko.relation.Repository.ClientRepository;
+import com.herve2iko.relation.dto.detailData;
+
+@RestController
+public class ClientController {
+    @Autowired
+    private ClientRepository clientRepository;
+
+
+    @PostMapping("/client")
+    public Client addClient(@RequestBody Client client){
+        return clientRepository.save(client);
+    }
+    @GetMapping("/all")
+    public List<Client>allClient(){
+        return clientRepository.findAll();
+    }
+    @GetMapping("/all-info")
+    public List<detailData>allInfo(){
+        return clientRepository.getJoinedInfo();
+    }
+}
