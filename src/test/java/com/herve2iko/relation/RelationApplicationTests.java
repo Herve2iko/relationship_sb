d04086e7c71f5723dfb93eb9ecad5fad4ed00401
Herve2iko <ihervedelice@gmail.com>
2022-06-21 15:03:14 +0200

    clean and clear project

---
+++ src/test/java/com/herve2iko/relation/RelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herve2iko.relation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
