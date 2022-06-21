0c80a32baf3801e6dbe88cbd3b5289f4b1c1db79
Herve2iko <ihervedelice@gmail.com>
2022-06-21 17:40:57 +0200

    detail data

---
+++ src/main/java/com/herve2iko/relation/dto/detailData.java
@@ -0,0 +1,15 @@
+package com.herve2iko.relation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class detailData {
+    private String nomCli;
+    private String nomProd;
+}
