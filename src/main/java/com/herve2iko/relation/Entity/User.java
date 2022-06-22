f740df50d1f5e73e43a2135448ec52084557e107
Herve2iko <ihervedelice@gmail.com>
2022-06-22 09:18:52 +0200

    user entity

---
+++ src/main/java/com/herve2iko/relation/Entity/User.java
@@ -0,0 +1,54 @@
+package com.herve2iko.relation.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    private Integer salary;
+    private String teamName;
+    
+    public User() {
+    }
+
+    public User(String name, Integer salary, String teamName) {
+        this.name = name;
+        this.salary = salary;
+        this.teamName = teamName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+    
+    
+}
